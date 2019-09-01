b0fb16dde51e7a4fda67858a2299bce2807af550
Rishav <rishav1209@gmail.com>
2019-09-01 16:58:48 +0530

    Add Spinner
    -Fix Temperature toggle
    -Preload Spinner Image
    -Added Styles
    -Copy Assets

--- src/weather-station.js
+++ src/weather-station.js
@@ -1,5 +1,6 @@
 import {getUserLocation} from './geolocation';
 import {WeatherService} from './weather.service';
+import { strict } from 'assert';
 
 
 export const tempUnits = {
@@ -20,9 +21,17 @@ export class WeatherStation {
     return getUserLocation()
     .then(position => this.weatherService.get(position.coords))
     .then(weather => {
+      if (weather) {
+        this.toggleImages();
+      }
       this.weather = weather;
       this.render()
     });
+  }
+
+  toggleImages() {
+    this.toggleHidden(this.weatherIcon);
+    this.toggleHidden(this.spinner);
   }
 
   getElementRefs() {
@@ -32,6 +41,7 @@ export class WeatherStation {
     this.title = document.getElementById('cardTitle');
     this.weatherDetails = document.getElementById('weatherDetails');
     this.weatherIcon = document.getElementById('weatherIcon');
+    this.spinner = document.getElementById('spinner');
   }
 
   weatherDetailsTemplate(unit = tempUnits.Farhenheit) {
@@ -39,7 +49,7 @@ export class WeatherStation {
     if (unit === tempUnits.Celsius) {
       temp = this.getTemp(tempUnits.Celsius);
     }
-    return `<p class="card-text">Current: ${temp.current} F</p>
+    return `<p class="card-text">Current: ${temp.current}</p>
             <p class="card-text">Humidity: ${this.weather.main.humidity}</p>
             <p class="card-text">Pressure: ${this.weather.main.pressure}</p>
             <p class="card-text">Max Temp: ${temp.max}</p>
@@ -55,19 +65,23 @@ export class WeatherStation {
   getTemp(unit) {
     if (unit === tempUnits.Celsius) {
       return {
-        current: this.toCelsius(this.weather.main.temp),
-        max: this.toCelsius(this.weather.main.temp_max),
-        min: this.toCelsius(this.weather.main.temp_min)
+        current: String(this.toCelsius(this.weather.main.temp)).concat(" C"),
+        max: String(this.toCelsius(this.weather.main.temp_max)).concat(" C"),
+        min: String(this.toCelsius(this.weather.main.temp_min)).concat(" C")
       }
-    }
+}
     return {
-      current: this.weather.main.temp,
-      max: this.weather.main.temp_max,
-      min: this.weather.main.temp_min
+      current: String(this.weather.main.temp).concat(" F"),
+      max: String(this.weather.main.temp_max).concat(" F"),
+      min: String(this.weather.main.temp_min).concat(" F")
     }
   }
 
   toCelsius(temp) {
-    return (temp - 32)*5/9;
+    return ((temp - 32)*5/9).toFixed(2);
+  }
+
+  toggleHidden(element) {
+    element.classList.toggle('hidden');
   }
 }
